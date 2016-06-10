acd43d47141bbd25d09959e17ad209b104823f13
Gavin Joyce <gavinjoyce@gmail.com>
2016-06-10 14:32:35 +0100

    import ember-computed-template-string-parser

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 /* jshint node: true */
 'use strict';
 
+var Parser = require('ember-computed-template-string-parser');
+var parser = new Parser('hi ${name}!');
+
+console.log('Computed Property:', parser.toComputedPropertyString());
+
 module.exports = {
   name: 'ember-computed-template-string'
 };
