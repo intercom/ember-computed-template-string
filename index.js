86e1f376bf56ff0a28e045428e5462240749705e
serenaf <serena@intercom.io>
2016-06-13 14:10:02 +0100

    using the template string parser

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 /* jshint node: true */
 'use strict';
 
-var Parser = require('ember-computed-template-string-parser');
-var parser = new Parser('hi ${name}!');
-
-console.log('Computed Property:', parser.toComputedPropertyString());
+var ComputedTemplateStringTransform = require('./plugins/computed-template-string-transform');
 
 module.exports = {
-  name: 'ember-computed-template-string'
+  name: 'ember-computed-template-string',
+  included: function(app) {
+    this._super.included(...arguments);
+    app.options = app.options || {};
+    app.options.babel = app.options.babel || {};
+    app.options.babel.plugins = app.options.babel.plugins || [];
+
+    if (!this._registeredWithBabel) {
+      app.options.babel.plugins.push(ComputedTemplateStringTransform());
+      this._registeredWithBabel = true;
+    }
+  }
+
 };
